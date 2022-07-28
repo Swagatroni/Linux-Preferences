353bc5c935eb0de687954426a6cf431e1957a261
Kenn Thomas <kennethwthomasjr@hotmail.com>
2022-07-28 00:14:20 -0500

    Blank

--- Packages.sh
+++ Packages.sh
@@ -7,6 +7,7 @@ cd ~/
 sudo apt-get install zsh -y
 chsh -s /usr/bin/zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
 sudo apt update
 sudo apt upgrade
