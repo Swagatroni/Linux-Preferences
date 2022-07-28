7b628f9118fea15bd7faf3d0dddf123eb0416204
Kenn Thomas <kennethwthomasjr@hotmail.com>
2022-07-27 22:15:20 -0500

    Blank

--- Packages.sh
+++ Packages.sh
@@ -6,38 +6,43 @@
 sudo apt update
 sudo apt upgrade
 
-# Install Powerlevel10k
+# Powerlevel10k
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
-# Install gh
-curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
-sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
-sudo apt update
-sudo apt install gh
-
-# Install Caffeine
-sudo apt -y install caffeine
-
-# Install Powerline
-sudo apt install --yes powerline
-
-# Install Python
-sudo apt install python3
-
-# Install Trash-CLI
-sudo apt install trash-cli
-
-# Install Sublime-Text
+# Sublime-Text
 sudo apt install dirmngr gnupg apt-transport-https ca-certificates software-properties-common
 curl -fsSL https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
 sudo add-apt-repository "deb https://download.sublimetext.com/ apt/stable/"
 sudo apt install sublime-text
 
-# Install Unimatrix
+# gh
+curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
+sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+sudo apt install gh
+
+
+# ZSH-Autosuggestions
+echo 'deb http://download.opensuse.org/repositories/shells:/zsh-users:/zsh-autosuggestions/xUbuntu_21.10/ /' | sudo tee /etc/apt/sources.list.d/shells:zsh-users:zsh-autosuggestions.list
+curl -fsSL https://download.opensuse.org/repositories/shells:zsh-users:zsh-autosuggestions/xUbuntu_21.10/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/shells_zsh-users_zsh-autosuggestions.gpg > /dev/null
+sudo apt install zsh-autosuggestions
+
+# Caffeine
+sudo apt -y install caffeine
+
+# Powerline
+sudo apt install --yes powerline
+
+# Python
+sudo apt install python3
+
+# Trash-CLI
+sudo apt install trash-cli
+
+# Unimatrix
 sudo curl -L https://raw.githubusercontent.com/will8211/unimatrix/master/unimatrix.py -o /usr/local/bin/unimatrix
 sudo chmod a+rx /usr/local/bin/unimatrix
 
 # Used to configure Powerlevel10k Theme
 p10k configure
-
+exec zsh
